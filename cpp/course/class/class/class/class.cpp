af894a73f460a9e2038d05502cb9f775dee2a1ce
cliff <mesh2433448@gmail.com>
2022-07-10 23:17:53 +0800

    add

--- cpp/course/class/class/class/class.cpp
+++ cpp/course/class/class/class/class.cpp
@@ -2,19 +2,56 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+	Person()
+	{
+		cout << "无参构造函数" << endl;
+
+	}
+	Person(int a)
+	{
+		age = a;
+		cout << "有参构造函数" << endl;
+	}
+	Person(const Person& p)
+	{
+		age = p.age;
+		cout << "拷贝构造函数" << endl;
+
+	}
+	~Person()
+	{
+		cout << "析构函数" << endl;
+	}
+	int age;
+};
+
+void test1()
+{
+	//括号法
+	//Person p1;//默认构造函数的调用
+	//Person p2(10);//有参构造函数
+	//Person p3(p2);//拷贝构造函数调用
+	//调用默认构造函数的时候不要加小括号
+	// 
+	//cout << "p2的年龄" << p2.age << endl;
+	//cout << "p3的年龄" << p3.age << endl;
+	//显示法
+	Person p1;
+	Person p2 = Person(10);
+	Person p3 = Person(p2);
+	//不要用拷贝构造函数来初始化匿名对象 Person(p3)
+	//隐式法
+	Person p4 = 10; //=== Person p4 = Person(10)
+	Person p5 = p4;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	test1();
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
