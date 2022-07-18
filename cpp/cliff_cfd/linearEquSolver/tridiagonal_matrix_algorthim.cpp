5c5e4662c4694fee4f0e9f7dbe53f8a7f413b4e7
che.shugang.cliff <che.shugang.cliff@vertiv.com>
2022-07-18 18:18:41 +0800

    daily

--- cpp/cliff_cfd/linearEquSolver/tridiagonal_matrix_algorthim.cpp
+++ cpp/cliff_cfd/linearEquSolver/tridiagonal_matrix_algorthim.cpp
@@ -8,18 +8,18 @@ using namespace arma;
 
 void solver_tdma(mat* a) {
 	cout << *a << endl;
-	vec cache = { 0,0,0,0 };//缓存项(a2,b2,c2)
+	vec vec_cache = { 0,0,0,0 };//缓存项
 	int len = a->n_rows;
-	//顺序消元依次
+	//1.顺序消元依次
 	for (int i = 1; i < len; i++) {				
-		cache = { a->at(i,0),a->at(i,1), a->at(i,2), a->at(i,3) };//缓存
+		vec_cache = { a->at(i,0),a->at(i,1), a->at(i,2), a->at(i,3) };//缓存
 		cout << *a << endl;
 		a->at(i, 0) = 0;
-		a->at(i, 1) = (a->at(i - 1, 1)) * cache.at(1)- cache.at(0)* (a->at(i - 1, 2));
-		a->at(i, 2) = cache.at(2) * (a->at(i - 1, 1));
-		a->at(i, 3) = a->at(i - 1, 1) * cache.at(3) - a->at(i - 1, 3) * cache.at(0);
+		a->at(i, 1) = (a->at(i - 1, 1)) * vec_cache.at(1)- vec_cache.at(0)* (a->at(i - 1, 2));
+		a->at(i, 2) = vec_cache.at(2) * (a->at(i - 1, 1));
+		a->at(i, 3) = a->at(i - 1, 1) * vec_cache.at(3) - a->at(i - 1, 3) * vec_cache.at(0);
 	}
-	//最后两列消掉一个元素
+	//2.最后两列消掉一个元素
 	
 	//逆序消元一次
 	
@@ -30,7 +30,7 @@ void solver_tdma(mat* a) {
 }
 
 void init_tdma() {
-	dmat a = {//简化存储三对角矩阵,对于大型三对角矩阵可以节省内存{a1,b1,c1,d1}
+	dmat a = {//简化存储三对角矩阵对应的线性方程组Ax = b,对于大型三对角矩阵可以节省内存{a,b,c,d}
 		{1.1 ,1.2 ,0.0 ,1},
 		{1.2 ,1.3 ,1.4 ,2},
 		{2.3 ,4.1 ,2.5 ,3},
@@ -40,13 +40,13 @@ void init_tdma() {
 	solver_tdma(&a);
 }
 
-/*	cout <<"cache"<< cache << endl;
+/*	cout <<"vec_cache"<< vec_cache << endl;
 	cout <<"a1: "<< a->at(i - 1, 0) << endl;
 	cout <<"b1: "<< a->at(i - 1, 1) << endl;
 	cout <<"c1: "<< a->at(i - 1, 2) << endl;
 	cout <<"d1: "<< a->at(i - 1, 3) << endl;
-	cout << "a2: " << cache.at(0) << endl;
-	cout << "b2: " << cache.at(1) << endl;
-	cout << "c2: " << cache.at(2) << endl;
-	cout << "d2: " << cache.at(3) << endl;
+	cout << "a2: " << vec_cache.at(0) << endl;
+	cout << "b2: " << vec_cache.at(1) << endl;
+	cout << "c2: " << vec_cache.at(2) << endl;
+	cout << "d2: " << vec_cache.at(3) << endl;
 	*/
