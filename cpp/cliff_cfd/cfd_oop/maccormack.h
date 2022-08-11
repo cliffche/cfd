008f621ca1d9835a1422e0eefc41d4cddfcefdbf
cliff <mesh2433448@gmail.com>
2022-08-12 01:07:40 +0800

    add

---
+++ cpp/cliff_cfd/cfd_oop/maccormack.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _MACCORMACK_H_
+#define _MACCORMACK_H_
+//maccormack推进方法,一维和二维问题
+class maccormack
+{
+public:
+	maccormack();
+	~maccormack();
+
+private:
+
+};
+
+maccormack::maccormack()
+{
+}
+
+maccormack::~maccormack()
+{
+}
+
+#endif // !_MACCORMACK_H_
+
