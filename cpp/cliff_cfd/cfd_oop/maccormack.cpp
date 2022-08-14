b36fe92f717a701438fd98dbbc42963ab53ed3e8
cliff <mesh2433448@gmail.com>
2022-08-14 23:57:56 +0800

    add

--- cpp/cliff_cfd/cfd_oop/maccormack.cpp
+++ cpp/cliff_cfd/cfd_oop/maccormack.cpp
@@ -1,1 +1,5 @@
 #include "maccormack.h"
+
+namespace MAC {
+
+}
--- cpp/cliff_cfd/cfd_oop/maccormack.h
+++ cpp/cliff_cfd/cfd_oop/maccormack.h
@@ -1,7 +1,10 @@
 #pragma once
 #ifndef _MACCORMACK_H_
 #define _MACCORMACK_H_
-//maccormack推进方法,一维和二维问题
+#include <map>
+#include <string>
+//maccormack推进方法,二维完整n-s方程
+
 class maccormack
 {
 public:
@@ -9,16 +12,17 @@ public:
 	~maccormack();
 
 private:
+	std::map<std::string, double> point;
 
 };
 
 maccormack::maccormack()
 {
-}
 
-maccormack::~maccormack()
-{
-}
+
+	maccormack::~maccormack()
+	{
+	}
 
 #endif // !_MACCORMACK_H_
 
